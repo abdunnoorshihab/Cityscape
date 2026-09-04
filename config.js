f36f9731a2c9507a30263b71a86715d3a3f21435
abdunnoorshihab <abdunnoorshihab@gmail.com>
2026-09-05 03:01:40 +0600

    Update Apps Script endpoint

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 // Paste the Google Apps Script Web App URL here after deploying backend/Code.gs.
-window.CITYSCAPE_FORM_ENDPOINT = "https://script.google.com/macros/s/AKfycbzt9PY8w_knVCRRgjBBg4WVp4LfQB0rB5w3BQstwY6-phs5H31t46OWyZnxQtfZ-4XL/exec";
+window.CITYSCAPE_FORM_ENDPOINT = "https://script.google.com/macros/s/AKfycbw0NixZ61MpnYMAt10KrUji3r53RKAQiLFnp0NzPopwE420-xCreP2CmUJI3HhfU0swvQ/exec";
